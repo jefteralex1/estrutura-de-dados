7c0e637ce92fe576d3fa05e22a2041173c71bcc3
Jefter Alexandre <103465537+jefteralex1@users.noreply.github.com>
2025-11-10 22:08:11 -0300

    Create ordenacao.h for Selection Sort implementation
    Add header file for Selection Sort algorithm with comparison count.

---
+++ ordenacao.h
@@ -0,0 +1,10 @@
+#ifndef ORDENACAO_H
+#define ORDENACAO_H
+
+/*
+ * Aplica o algoritmo Selection Sort.
+ * Conta o número de comparações e o retorna através do ponteiro 'comparacoes'.
+ */
+void selectionSort(int vetor[], int tam, long *comparacoes);
+
+#endif
